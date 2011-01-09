f60f39c70bbdc43727c3a176b75f90f260f99512
Theo-K <tr.klein@tvm.nl>
2011-01-09 12:03:23 +0100

    Dutch translation added

---
+++ lang/nl/lang.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Dutch translation
+ * @author Theo-K <tr.klein@tvm.nl>
+ */
+
+$lang['last']  = 'Laatste pagina';
+$lang['next']  = 'Volgende pagina';
+$lang['up']    = 'Terug naar bovenliggende pagina';
+$lang['prev']  = 'Vorige pagina';
+$lang['first'] = 'Eerste pagina';
