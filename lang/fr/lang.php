7ce405a22e39ad4ed8c223f144d88f54a6976e5c
Alphonse Philippe <a.philippe@wanadoo.fr>
2012-01-31 10:09:01 +0100

    French translation

---
+++ lang/fr/lang.php
@@ -0,0 +1,7 @@
+<?php
+
+$lang['last']  = 'Dernière page ';
+$lang['next']  = 'Page suivante ';
+$lang['up']    = 'Niveau supérieur ';
+$lang['prev']  = 'Page précédente ';
+$lang['first'] = 'Première page ';
