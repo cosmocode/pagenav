c4553553ca16b147a643583dd2bb739ac0669ca9
Hideaki SAWADA <chuno@live.jp>
2014-09-23 04:29:43 +0900

    Create lang.php

---
+++ lang/ja/lang.php
@@ -0,0 +1,13 @@
+<?php
+ 
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Hideaki SAWADA <chuno@live.jp>
+ */
+
+$lang['last']  = '最後のページ';
+$lang['next']  = '次のページ';
+$lang['up']    = '概要に戻る';
+$lang['prev']  = '前のページ';
+$lang['first'] = '最初のページ';
